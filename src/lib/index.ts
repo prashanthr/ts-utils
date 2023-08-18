1757a9ec868946367e4e78bbee56cf22477c8284
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2023-08-18 17:55:25 +0000

    fix build

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,17 @@
 // Result
-export type { Result, Ok, Err, Matchers as ResultMatchers } from './result'
-export { ok, err, match as matchResult, matchF as matchResultF } from './result'
+export type { Result, Ok, Err, Matchers as ResultMatchers } from "./result";
+export {
+  ok,
+  err,
+  match as matchResult,
+  matchF as matchResultF,
+} from "./result";
 
 // Either
-export type { Either, Right, Left, Matchers as EitherMatchers } from './either'
-export { right, left, match as matchEither, matchF as matchEitherF } from './either'
+export type { Either, Right, Left, Matchers as EitherMatchers } from "./either";
+export {
+  right,
+  left,
+  match as matchEither,
+  matchF as matchEitherF,
+} from "./either";
