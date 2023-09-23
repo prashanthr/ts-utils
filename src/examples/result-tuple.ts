d1078a6d914f3290ae1a26854fc87200472c7711
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2023-09-22 23:10:12 -0700

    feat(result-tuple): Add result-tuple lib

---
+++ src/examples/result-tuple.ts
@@ -0,0 +1,43 @@
+import { MaybeResult, MaybeError, ResultTuple, toTuple } from '../lib'
+
+const maybeResult: MaybeResult<number> = 1
+const maybeResult2: MaybeResult<number> = undefined
+const maybeResult3: MaybeResult<number> = null
+const maybeResult4: MaybeResult<number, 'bar'> = 'bar'
+
+const maybeError: MaybeError = new Error('e')
+const maybeError2: MaybeError = undefined
+const maybeError3: MaybeError<Error> = new Error('e')
+const maybeError4: MaybeError<Error> = undefined
+const maybeError5: MaybeError<Error, 'bar'> = 'bar'
+
+class CustomError extends Error {
+    constructor(msg: string) {
+        super(msg)
+        Object.setPrototypeOf(this, CustomError.prototype)
+    }
+}
+const maybeError6: MaybeError<CustomError> = new CustomError('custom error')
+
+const resultTuple: ResultTuple<number> = [1, undefined]
+const resultTuple2: ResultTuple<number, Error> = [1, undefined]
+const resultTuple3: ResultTuple<number> = [undefined, new Error('e')]
+const resultTuple4: ResultTuple<number, Error> = [undefined, new Error('e')]
+const resultTuple5: ResultTuple<number, Error, 'baz'> = [1, 'baz']
+const resultTuple6: ResultTuple<number, Error, 'baz'> = ['baz', new Error('e')]
+
+const fn = (): ResultTuple<number> => {
+    return toTuple({ result: 1 }) // [1, undefined]
+}
+
+const fn2 = (): ResultTuple<number> => {
+    return toTuple({ result: 1, error: undefined }) // [1, undefined]
+}
+
+const fn3 = (): ResultTuple<number> => {
+    return toTuple({ error: new Error('e') }) // [undefined, Error('e')]
+}
+
+const fn4 = (): ResultTuple<number> => {
+    return toTuple({}) // [undefined, undefined]
+}
